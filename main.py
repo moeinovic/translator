ad2f79c7ec2f687b47623271d843fed269a0b5e0
Moeinovic <76170229+moeinovic@users.noreply.github.com>
2021-12-19 22:03:11 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from aiohttp import ClientSession
+from Python_ARQ import ARQ
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from uvicorn import run
+app = FastAPI()
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"ok":False, "detail": exc.errors()}),
+    )
+
+
+
+
+async def main(text: str, lang: str):
+    session = ClientSession()
+    arq = ARQ("https://thearq.tech", "JCOBCX-IEYTGE-AZFIEP-AYLDTF-ARQ", session)
+    results = await arq.translate(text, lang)
+    await session.close()
+    return results
+@app.get("/translate/")
+async def root(text: str, lang: str):
+    myres = await main(text, lang)
+    return {"ok":True, "data" : myres}
+
+if __name__ == '__main__':
+    run(app, port=80, host='127.0.0.1')
